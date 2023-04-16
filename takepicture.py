1e5b9547e2eb6f82f3399ba5bb75cedc00bcc045
yamukadam <85646755+yamukadam@users.noreply.github.com>
2023-04-16 02:16:06 -0500

    Update takepicture.py

--- takepicture.py
+++ takepicture.py
@@ -1,17 +1,4 @@
 import cv2
-
-# Windows dependencies
-# - Python 2.7.6: http://www.python.org/download/
-# - OpenCV: http://opencv.org/
-# - Numpy -- get numpy from here because the official builds don't support x64:
-#   http://www.lfd.uci.edu/~gohlke/pythonlibs/#numpy
-
-# Mac Dependencies
-# - brew install python
-# - pip install numpy
-# - brew tap homebrew/science
-# - brew install opencv
-
 cap = cv2.VideoCapture(0)
 
 while(True):
@@ -25,4 +12,3 @@ while(True):
 
 cap.release()
 cv2.destroyAllWindows()
-
